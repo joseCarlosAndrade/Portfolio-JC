a6c61cb0ca5fedb828c01a0e065fa1b48884a4e9
joseCarlosAndrade <josecarlos.101@usp.br>
2024-03-16 19:55:25 -0300

    initial site

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <>
+    <div className='headerContainer'>
+        <div className='headerContainer--content'>
+            Header
+        </div>
+        
+    </div>
+    </>
+  )
+}
+
+export default Header
