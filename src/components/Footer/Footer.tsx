189b04fb5d8ae3512a05c07be9c10d0e564f5a12
joseCarlosAndrade <josecarlos.101@usp.br>
2024-04-22 18:14:59 -0300

    footer done responsivness

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,16 +9,17 @@ function Footer() {
     <>
         <div className='footerContainer'>
           <div className='footerContainer--items'>
-            {/* <div className='footerContainer--items--social' ></div> */}
-            <a target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/jos%C3%A9-carlos-andrade-do-nascimento-71186421a/">
-              <img className='transitionEase scaleOnHover105' src={linkedinIcon} alt="Linkedin icon" /> 
-            </a>
-            <a target='_blank' rel='noopener noreferrer' href="https://github.com/joseCarlosAndrade">
-              <img className='transitionEase scaleOnHover105' src={githubIcon} alt="Github icon" />
-            </a>
-            <a target='_blank' rel='noopener noreferrer' href="https://www.instagram.com/jc.andrade.n/">
-              <img className='transitionEase scaleOnHover105' src={instagramIcon} alt="Instagram icon" />
-            </a>
+            <div className='footerContainer--items-social'>
+              <a target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/jos%C3%A9-carlos-andrade-do-nascimento-71186421a/">
+                <img className='transitionEase scaleOnHover105' src={linkedinIcon} alt="Linkedin icon" /> 
+              </a>
+              <a target='_blank' rel='noopener noreferrer' href="https://github.com/joseCarlosAndrade">
+                <img className='transitionEase scaleOnHover105' src={githubIcon} alt="Github icon" />
+              </a>
+              <a target='_blank' rel='noopener noreferrer' href="https://www.instagram.com/jc.andrade.n/">
+                <img className='transitionEase scaleOnHover105' src={instagramIcon} alt="Instagram icon" />
+              </a>
+            </div>
             <div className='footerContainer--items--divider' ></div>
             <div className='footerContainer--items--name'>Made by Jose Carlos Andrade do Nascimento</div>
             
